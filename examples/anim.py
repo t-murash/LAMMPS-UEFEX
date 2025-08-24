3883b3491d2c800094201e0a63b4101923625692
t-murash <takamura1978@gmail.com>
2025-08-25 02:11:51 +0900

    22Jul2025

--- examples/anim.py
+++ examples/anim.py
@@ -1,8 +1,9 @@
-step=27
+step=100
 frame0=str(step).zfill(3)
+name='ring.N400'
 print(frame0)
 from ovito.io import import_file
-filename="N100M100."+str(frame0)+".data"
+filename=name+'.'+str(frame0)+".data"
 pipeline=import_file(filename,atom_style="bond")
 
 data=pipeline.compute()
@@ -50,75 +51,52 @@ def color_rgb(i,max):
     return [r,g,b]
 
 def modify(frame,data):
-    color=data.particles_.create_property('Color')
-    #trans=data.particles_.create_property('Transparency')
-    for i in range(num_particles):
-        mol_i=mol[i]
-        #if mol_i == 1:
-        #    trans[i]=0.0
-        #else:
-        #    trans[i]=0.9
-        color[i]=color_rgb(mol_i,mol_max)
-
-
-    pbc=data.particles_['Periodic Image_']
-    pid=data.particles['Particle Identifier']
-    mol_id_min=np.zeros((mol_max))
-    mol_id_max=np.zeros((mol_max))
-    mol_id_mid=np.zeros((mol_max))
-    pbc_mid=np.zeros((mol_max,3))
-    pbc_sort=np.zeros((num_particles,3))
-    for i in range(mol_max):
-        mol_id_min[i]=2*num_particles
-        mol_id_max[i]=-1
-
-    for i in range(num_particles):
-        pid_i=pid[i]
-        mol_i=mol[i]
-        if pid_i < mol_id_min[mol_i-1]:
-            mol_id_min[mol_i-1]=pid_i
-        if pid_i > mol_id_max[mol_i-1]:
-            mol_id_max[mol_i-1]=pid_i
-
-    for i in range(num_particles):
-        pid_i=pid[i]
-        pbc_sort[pid_i-1,0]=pbc[i,0]
-        pbc_sort[pid_i-1,1]=pbc[i,1]
-        pbc_sort[pid_i-1,2]=pbc[i,2]
-
-    for i in range(mol_max):
-        min_id=mol_id_min[i]
-        max_id=mol_id_max[i]
-        mol_id_mid[i]=np.ceil((max_id-min_id+1)/2)+min_id
-        pbc_mid[i,0]=pbc_sort[int(mol_id_mid[i])-1,0]
-        pbc_mid[i,1]=pbc_sort[int(mol_id_mid[i])-1,1]
-        pbc_mid[i,2]=pbc_sort[int(mol_id_mid[i])-1,2]
-
-    for i in range(num_particles):
-        pid_i=pid[i]
-        mol_i=mol[i]
-        pbc[i,0]-=pbc_mid[mol_i-1,0]
-        pbc[i,1]-=pbc_mid[mol_i-1,1]
-        pbc[i,2]-=pbc_mid[mol_i-1,2]
-
+     color=data.particles_.create_property('Color')
+     #trans=data.particles_.create_property('Transparency')
+     for i in range(num_particles):
+         mol_i=mol[i]
+         #if mol_i == 1:
+         #    trans[i]=0.0
+         #else:
+         #    trans[i]=0.9
+         color[i]=color_rgb(mol_i,mol_max)
 pipeline.modifiers.append(modify)
+
 
 from ovito.modifiers import UnwrapTrajectoriesModifier
 pipeline.modifiers.append(UnwrapTrajectoriesModifier())
 
-#
-#def modify_unwrap(frame,data):
-#    pos=data.particles_['Position_']
-#    opos=data.particles_['Position_']
-#    pbc=data.particles['Periodic Image']
-#    molid=data.particles['Molecule Identifier']
-#    cell=data.cell
-#    NM=molid.size
-#    for i in range(NM):
-#        for k in range(3):
-#            opos[i,k]=pos[i,k]+cell[k,0]*pbc[i,0]+cell[k,1]*pbc[i,1]+cell[k,2]*pbc[i,2]-cell[k,3]
-#pipeline.modifiers.append(modify_unwrap)
-#
+
+def modify_pbc(frame,data):
+    pid=data.particles['Particle Identifier']
+    pos=data.particles_['Position_']
+    opos=data.particles_['Position_']
+    cell=data.cell
+    com=np.zeros((num_particles,3))
+    com2=np.zeros((num_particles,3))
+    com_pbc=np.zeros((mol_max,3),dtype=int)
+    com_pbc_s=np.zeros((mol_max,3))
+    num_per_mol=np.zeros(mol_max,dtype=int)
+    H=np.zeros((3,3))
+    for i in range(3):
+        for j in range(3):
+            H[i,j]=cell[i,j]
+    Hinv=np.linalg.inv(H)
+    for i in range(num_particles):
+        pid_i=pid[i]-1
+        mol_i=mol[i]-1
+        com[mol_i]+=pos[i]
+        num_per_mol[mol_i]+=1
+    for i in range(mol_max):
+        com[i]/=num_per_mol[i]
+    for i in range(mol_max):
+        com_pbc[i]=np.ceil(np.dot(Hinv,com[i])-0.5)
+    for i in range(num_particles):
+        mol_i=mol[i]-1
+        opos[i]=pos[i]-np.dot(H,com_pbc[mol_i])
+
+pipeline.modifiers.append(modify_pbc)    
+
 
 def modify_rotation(frame,data):
     cell=data.cell
@@ -168,13 +146,13 @@ def modify_rotation(frame,data):
     r_tmp=np.zeros(3)
     r_rot=np.zeros(3)
     for i in range(NM):
-        r_tmp[0]=ipos[i,0]
-        r_tmp[1]=ipos[i,1]
-        r_tmp[2]=ipos[i,2]
+        r_tmp[0]=ipos[i,0]-org[0]
+        r_tmp[1]=ipos[i,1]-org[1]
+        r_tmp[2]=ipos[i,2]-org[2]
         vec_rot(rot,r_tmp,r_rot)
-        opos[i,0]=r_rot[0]
-        opos[i,1]=r_rot[1]
-        opos[i,2]=r_rot[2]
+        opos[i,0]=r_rot[0]+org[0]
+        opos[i,1]=r_rot[1]+org[1]
+        opos[i,2]=r_rot[2]+org[2]
     
 pipeline.modifiers.append(modify_rotation)
 
@@ -204,9 +182,9 @@ lz=data.cell[2,2]
 import math
 from ovito.vis import Viewport
 vp = Viewport(type=Viewport.Type.Perspective)
-vp.camera_pos=(0,-220,20)
+vp.camera_pos=(0,-420,20)
 vp.camera_dir=(0,1,0)
 vp.overlays.append(tripod)
 from ovito.vis import TachyonRenderer
-vp.render_image(size=(500,500),filename="figure."+str(frame0)+".png",background=(1.0,1.0,1.0),renderer=TachyonRenderer())
+vp.render_image(size=(1600,1600),filename="figure."+str(frame0)+".png",background=(1.0,1.0,1.0),renderer=TachyonRenderer())
 
